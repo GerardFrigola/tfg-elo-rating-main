ca0f7c4e3daf5f7b3b8d645b56f3d39d8067460f
FRIGO <1606221@uab.cat>
2025-05-03 11:19:13 +0200

    Fix theming

--- web/web.py
+++ web/web.py
@@ -18,22 +18,6 @@ players = pd.read_csv('web/web_data/clean_atp_players.csv')
 assert matches.isna().sum().sum() == 0, f'nan values in matches\n{matches.isna().sum()}'
 assert players.isna().sum().sum() == 0, f'nan values in players\n{players.isna().sum()}'
 
-
-# Sidebar ---------------------------
-with st.sidebar:
-    st.title('Filtres')
-
-    k = st.number_input('Input K-factor', min_value=1, max_value=200, step=1, value=24, placeholder='Enter a integer')
-    ksi = st.number_input('Input xi', min_value=100, max_value=1000, step=1, value=400, placeholder='Enter a integer')
-    s = st.selectbox('Input score type:', options=['delta', 'thirds'], index=0)
-    show_surfaces = st.checkbox('Show surface ratings', value=False)
-    initial_elo = st.number_input('Initial Elo rating', min_value=0, max_value=5000, step=1, value=1500, placeholder='Enter a integer')
-
-    years = list(range(1970, 2025))
-    # year_list = st.multiselect('Select the year you want to see:', options=years)
-
-    run_simulation = st.button('Run simulation', key='run_simulation')
-
 # Functions -------------------------
     
 def simulate_tour(tour_df:pd.DataFrame, k, ksi, s, initial_elo): 
@@ -126,6 +110,25 @@ def simulate_tour(tour_df:pd.DataFrame, k, ksi, s, initial_elo):
     ranking.index += 1
 
     return ranking
+
+def values_changed():
+    st.write('Values have changed. Rerun the simulation to see the new results.')
+
+# Sidebar ---------------------------
+with st.sidebar:
+    st.title('Filtres')
+
+    k = st.number_input('Input K-factor', min_value=1, max_value=200, step=1, value=24, placeholder='Enter a integer', on_change=values_changed)
+    ksi = st.number_input('Input xi', min_value=100, max_value=1000, step=1, value=400, placeholder='Enter a integer', on_change=values_changed)
+    s = st.selectbox('Input score type:', options=['delta', 'thirds'], index=0, on_change=values_changed)
+    show_surfaces = st.checkbox('Show surface ratings', value=False)
+    initial_elo = st.number_input('Initial Elo rating', min_value=0, max_value=5000, step=1, value=1500, placeholder='Enter a integer', on_change=values_changed)
+
+    years = list(range(1970, 2025))
+    # year_list = st.multiselect('Select the year you want to see:', options=years)
+
+    run_simulation = st.button('Run simulation', key='run_simulation')
+
 
 
 
